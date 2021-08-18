65043aa02a1d71861702e3a0854826df6f54db13
rookie1024 <rookie1286@gmail.com>
2021-08-18 13:09:24 -0700

    Log redis endpoints for diagnostics.

--- rpc-cache-utils/src/redisClient.ts
+++ rpc-cache-utils/src/redisClient.ts
@@ -1,16 +1,22 @@
 import redis from "redis";
 
 export enum RedisClientUser {
-	Reader,
-	Writer
-};
+  Reader,
+  Writer,
+}
 
-export const getRedisClient = (clientType : RedisClientUser): redis.RedisClient => {
+export const getRedisClient = (
+  clientType: RedisClientUser
+): redis.RedisClient => {
   if (process.env.ENV?.toLowerCase() === "aws") {
-    return redis.createClient(
-      clientType === RedisClientUser.Reader ? `${process.env.REDIS_SERVER_READ_URL}:${process.env.REDIS_SERVER_PORT}` :
-	      				      `${process.env.REDIS_SERVER_PRIMARY_URL}:${process.env.REDIS_SERVER_PORT}`
-    );
+    const url =
+      clientType === RedisClientUser.Reader
+        ? `${process.env.REDIS_SERVER_READ_URL}:${process.env.REDIS_SERVER_PORT}`
+        : `${process.env.REDIS_SERVER_PRIMARY_URL}:${process.env.REDIS_SERVER_PORT}`;
+
+    console.log(`Connecting to redis via '${url}'...`);
+
+    return redis.createClient(url);
   } else {
     return redis.createClient();
   }
